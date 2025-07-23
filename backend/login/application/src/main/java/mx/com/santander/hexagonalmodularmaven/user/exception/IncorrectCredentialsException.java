2e882650b6c98fbbc86489cbd5d14eaa895a7f7c
Rommel Dev/c/Users/SoporteTI/Documents/WORKSPACE-MSC/PAYMENTCHAIN EJEMPLO/paymentchainparent3/businessdomain/customer <rommelkd36@gmail.com>
2025-07-23 13:30:35 -0600

    se borra login

--- backend/login/application/src/main/java/mx/com/santander/hexagonalmodularmaven/user/exception/IncorrectCredentialsException.java
+++
@@ -1,8 +0,0 @@
-package mx.com.santander.hexagonalmodularmaven.user.exception;
-
-public class IncorrectCredentialsException extends RuntimeException {
-	public IncorrectCredentialsException() {
-		super("Las credenciales ingresadas no son correctas");
-	}
-
-}
